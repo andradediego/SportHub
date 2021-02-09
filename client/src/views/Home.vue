f7f7288cdfb728ba7b5637bc0e59b6a23c3a19dd
Diego Andrade <andrade.diego@outlook.com>
2021-02-09 16:01:22 -0500

    Added vuetify

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,10 +9,11 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld    
   }
 }
 </script>
