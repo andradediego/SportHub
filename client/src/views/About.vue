46936ca538f0a3c59e816a459e26ea9920fa9581
Diego Andrade <andrade.diego@outlook.com>
2021-02-23 22:11:49 -0500

    polished about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,70 +1,65 @@
 <template>
-<v-app>
-    <v-container>
-        <div class="text">
-        <h1 >About Us</h1>
-        <h2 class="title1">Our Vision</h2>
-        <div>We would like to create and organize sporting events for friends and families in the community </div>
-        <h2 class="title1" >Our Product</h2>
-        </div>
-    <div class="product" >
-     <ul>
-         <li> 
-        <v-card
-        class="mx-auto"
-        max-width="344">
-        <v-card-text>
-            <div>The app is specifically designed for sporting events</div>
-        </v-card-text>
-        </v-card>
-         </li>
-         <li> <v-card
-        class="mx-auto"
-        max-width="344">
-        <v-card-text>
-            <div>Facilitating the process of finding free </div>
-        </v-card-text>
-        </v-card></li>
-         <li> <v-card
-        class="mx-auto"
-        max-width="344">
-        <v-card-text>
-            <div>Booking sports fields available</div>
-        </v-card-text>
-        </v-card></li>
-         <li> <v-card
-        class="mx-auto"
-        max-width="344">
-        <v-card-text>
-            <div>Setting a date for the events </div>
-        </v-card-text>
-        </v-card></li>
-         <li> <v-card
-        class="mx-auto"
-        max-width="344">
-        <v-card-text>
-            <div>Finding partners and letting them join the games without a teams</div>
-        </v-card-text>
-        </v-card></li>
-     </ul>    
-</div>
-  <v-img 
-         max-width="100%"
-  src="https://cdn.dribbble.com/users/4107928/screenshots/11999532/media/1f6e258bac438f702804c334539cbaae.jpg?compress=1&resize=800x600"
-  ></v-img>
-
-    </v-container>
-</v-app>
-    
-
+   <v-row>
+       <v-col
+        cols="12"
+        xs="12"
+        sm="8"
+        md="6"
+        offset-sm="2"
+        offset-md="3"
+       >
+        <div class="text-h3 text-center">About Us</div>
+        <br>
+        <div class="text-h4 text-center">Our Vision</div>
+        <br>
+        <p class="text-center">We would like to create and organize sporting events for friends and families in the community</p>
+        <br>
+        <div class="text-h4 text-center">Our Product</div>
+        <br>
+        <v-row>            
+            <template v-for="product in products" > 
+                <v-col
+                    cols="12"
+                    xs="12"
+                    sm="6"                    
+                    :key="product"
+                    >           
+                <v-card                
+                    dark            
+                    color="#13893f"
+                    class="mx-auto pa-2"                    
+                    width="500">
+                    <v-card-text>
+                        <div >{{product}}</div>
+                    </v-card-text>
+                </v-card>
+                </v-col>
+            </template>
+        </v-row>
+        <v-img 
+            max-width="100%"
+            src="https://cdn.dribbble.com/users/4107928/screenshots/11999532/media/1f6e258bac438f702804c334539cbaae.jpg?compress=1&resize=800x600">
+        </v-img>
+       </v-col>
+   </v-row>
 </template>
+
 <script>
 export default {
 name: 'About',
+data: () => ({
+    products: [
+        'The app is specifically designed for sporting events',
+        'Facilitating the process of finding free sports fields',
+        'Booking sports fields available',
+        'Setting a date for the events',
+        'Finding partners and letting them join the games without a teams'
+    ]
+})
 }
 </script>
 <style>
-.text{
+/* .text{
 text-align: center;
 padding-top: 5%;
 }
@@ -95,6 +90,6 @@ li:hover{
 .product{
     display: flex;
     justify-content: space-between;
-}
+} */
 
 </style>
