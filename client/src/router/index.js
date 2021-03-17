28098728f1e77ff0d66a1a52da44d09d6225dba2
Oleksii Denyshchyk <63901283+dnshk@users.noreply.github.com>
2021-03-17 19:20:10 -0400

    Update index.js
    added calendar router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -54,7 +54,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Schedulle.vue')
+  },{
+    path: '/calendar',
+    name: 'calendar',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Calendar.vue')
   }
+  
 ]
 
 const router = new VueRouter({
