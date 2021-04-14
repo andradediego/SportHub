05768ab634ffb4c336ca7fbab0bcaeaef6858749
ngocdang525 <nguyentraik963@gmail.com>
2021-04-13 23:40:11 -0400

    sprt3

--- client/src/views/BookingField.vue
+++ client/src/views/BookingField.vue
@@ -1,17 +1,6 @@
 <template >
           <div class="bookingField">
-        <!-- <v-img
-            :src="field.src"
-            height="200px"            
-        ></v-img>
-        
-            <div>{{field.Name}}</div>
-        
-            <div>{{field.Location}}</div>
-        
-        
-            <div>{{field.Description}}</div>
-         -->
+      
          {{ $route.params.id }}
         
   </div>
